d9f671a47749bb5e61a13abe04c3386d749b84d1
vncebrmjo <bermejovince31@gmail.com>
2026-09-03 00:55:33 +0800

    -Updated landing-page unit test -Updated login unit test -Updated product-cards unit test

--- src/app/pages/landing-page/landing-page.spec.ts
+++ src/app/pages/landing-page/landing-page.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { LandingPage } from './landing-page';
 import { ProductCategoryService } from '../../core/services/product-category.service';
+import { ProductService } from '../../core/services/product.service';
 import { of } from 'rxjs';
 
 describe('LandingPage', () => {
@@ -9,10 +11,14 @@ describe('LandingPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingPage]
+      imports: [LandingPage],
+      providers: [provideRouter([])]
     })
     .overrideProvider(ProductCategoryService, {
       useValue: { getAllProductCategory: () => of([]) }
+    })
+    .overrideProvider(ProductService, {
+      useValue: { getAllProducts: () => of([]) }
     })
     .compileComponents();
 
